61f4a956ab0a54f08c38e59415a9c6588cb169fc
vonirise <masumifelix7@gmail.com>
2025-05-07 11:09:20 +0800

    mulai dari sini

---
+++ menu.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+void dMenu(){
+system("cls");
+cout<<"Aplikasi Tampilan Menu"<<"\n";       
+cout<<"1. Menu Pertama"<<"\n";            
+cout<<"2. Menu Kedua"<<"\n";            
+cout<<"3. Menu Ketiga"<<"\n";           
+cout<<"4. Menu Keempat"<<"\n";            
+cout<<"5. Exit"<<"\n";           
+cout<<"Masukan angka :";        
+
+}
+
+void mPertama(string pesan){
+system("cls");
+cout<<"hallo saya menu "<<pesan;
+getch();
+}
+
+
+int main() {
+char pl;
+do
+{
+    dMenu();
+    pl=getch();
+  switch (pl)
+  {
+   case '1':
+    /* code */
+    mPertama("pertama");
+    break;
+   case '2':
+    mPertama("ke- dua");
+    /* code */ 
+    break;  
+   case '3':
+    mPertama("ke- tiga");
+    /* code */
+    break;  
+   case '4':
+    mPertama("ke- empat");
+    /* code */
+    break;  
+  case '5':
+    /* code */
+    break;
+  
+  default:
+    system("cls");
+    cout<<"Pilihan Tidak Tersedia";
+    getch();
+    break;
+  }
+
+
+} while (pl!='5');
+  return 0;
+}
